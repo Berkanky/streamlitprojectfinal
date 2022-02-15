3f8eef9fe5688f01f602323766b43b9b83a0db86
Berkan <94999607+Berkanky@users.noreply.github.com>
2022-02-15 18:22:17 +0300

    Update main.py
    Update for graphs

--- main.py
+++ main.py
@@ -68,13 +68,14 @@ with col1:
     yearsec=st.number_input("Year",minyear,maxyear,value=2021)
     gendersec=st.checkbox("Gender(Default=All)")
     profitablesec=st.checkbox("Profitable (Default=All)")
-    #ınd=newdf.set_index("industry")
-    #ınds=ınd.index
-    #ınds=list(ınds.unique())
-    #ındustryst=st.multiselect("Enter Name Of Industry",ınds)
-    #if ındustryst:
-        #newdf=ınd.loc[ındustryst]
-        #newdf=newdf.reset_index()
+    jobgrowthst=st.checkbox("JobGrowth (Default=All)")
+            #ınd=newdf.set_index("industry")
+            #ınds=ınd.index
+            #ınds=list(ınds.unique())
+            #ındustryst=st.multiselect("Enter Name Of Industry",ınds)
+            #if ındustryst:
+                #newdf=ınd.loc[ındustryst]
+                #newdf=newdf.reset_index()
     if profitablesec:
         newdf=newdf[newdf["profitable"]=="no"]
         st.write("Only NoN Profitable")
@@ -85,6 +86,9 @@ with col1:
         newdf=newdf[newdf["rank"]>=ranksec]
     if yearsec:
         newdf=newdf[newdf["year"]==yearsec]
+    if jobgrowthst:
+        newdf=newdf[newdf["jobgrowth"]=="no"]
+        st.write("Only NoN Jobgrowth")
 with col2:
     st.dataframe(newdf)
     @st.cache
@@ -97,5 +101,32 @@ st.download_button(
     file_name='Fortune500.csv',
     mime='text/csv',
 )
-fig=px.bar(newdf,x=newdf["sector"],y=newdf["profits"])
+df=pd.read_csv("fortune.csv")
+df["revchange"]=df["revchange"].fillna(0)
+df["prftchange"]=df["prftchange"].fillna(0)
+df=df.drop(columns=["hqstate","permalink"])
+col1,col2=st.columns(2)
+with col1:
+    dfcolumns=["sector","industry","hqcity"]
+    stfirst=st.selectbox("Select First Column",dfcolumns)
+with col2:
+    dfcolumns2=["revenues","revchange","profits","prftchange","assets","employees"]
+    stsecond=st.selectbox("Select Second Column",dfcolumns2)
+with col2:
+    dfyear=list(df["year"].unique())
+    dfyear.insert(0,"All Years")
+    dfyearst=st.selectbox("Select Year",dfyear)
+    if dfyearst!="All Years":
+        df=df[df["year"]==dfyearst]
+df=df.groupby(stfirst)[stsecond].mean()
+df=df.reset_index()
+df=df.sort_values(by=[stsecond],ascending=False)
+graphlist=["Pie Graph","Bar Graph"]
+with col1:
+    graphst=st.selectbox("Select Graph",graphlist)
+if graphst=="Pie Graph":
+    fig=px.pie(df,values=stsecond,names=stfirst,title=stfirst,height=650)
+if graphst=="Bar Graph":
+    fig=px.bar(df,x=stfirst,y=stsecond,title=stfirst)
 st.plotly_chart(fig,use_container_width=True)
+#st.dataframe(df)
